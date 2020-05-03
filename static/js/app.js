db5d850568857e927a024c762228484bfcd6a6c0
KotR9001 <57807780+KotR9001@users.noreply.github.com>
2020-05-03 16:31:32 -0500

    Make changes.

--- static/js/app.js
+++ static/js/app.js
@@ -37,7 +37,7 @@ unpack();
 
 
 
-//Create the Charts
+//Create the Initial Charts & Demographic Panel
 function init() {d3.json('../../samples.json').then((data)=> {
     //Unpack the Values
     var names = data.names;
@@ -64,6 +64,15 @@ function init() {d3.json('../../samples.json').then((data)=> {
     console.log(sample_values_top10);
     console.log(otu_ids_top10);
 
+    //Put the Initial Values in the Demographic Panel
+    d3.select('.panel-body').append('p').html(`id: ${id[0]}`);
+    d3.select('.panel-body').append('p').html(`ethnicity: ${ethnicity[0]}`);
+    d3.select('.panel-body').append('p').html(`gender: ${gender[0]}`);
+    d3.select('.panel-body').append('p').html(`age: ${age[0]}`);
+    d3.select('.panel-body').append('p').html(`location: ${location[0]}`);
+    d3.select('.panel-body').append('p').html(`bbtype: ${bbtype[0]}`);
+    d3.select('.panel-body').append('p').html(`wfreq: ${wfreq[0]}`);
+
     //Create the Traces
     //Method to Add Hover Text Found at https://plotly.com/javascript/hover-text-and-formatting/
     //Bar
@@ -79,24 +88,26 @@ function init() {d3.json('../../samples.json').then((data)=> {
     };
 
     //Method to Make Bubble Chart Trace found at https://plotly.com/javascript/bubble-charts/
-    var size = sample_values_top10[0];
-    var desired_maximum_marker_size = 1;
+    //Learned more about SizeRef at https://github.com/plotly/documentation/issues/776
+    //Learned more about ColorScale at https://stackoverflow.com/questions/46648135/colorscale-mismatches-colors-in-plotly-python
+    var size = sample_values[0];
     trace_bubble = {
         mode: 'markers',
-        x: otu_ids_top10_1[0],
-        y: sample_values_top10[0],
-        text: otu_labels_top10,
+        x: otu_ids[0],
+        y: sample_values[0],
+        text: otu_labels[0],
         marker: {
-            color: otu_ids_top10_1[0],
+            color: otu_ids[0],
+            colorscale: [[0, 'blue'], [0.2, 'cyan'], [0.4, 'green'], [0.6, 'yellow'], [0.8, 'orange'], [1,'red']],
             size: size,
-            sizeref: 4.0 * Math.max(size) / (desired_maximum_marker_size**2),
-            sizemode: 'area'
+            sizeref: 2,
+            sizemode: 'diameter',
+            showscale: true
         }
     };
     //Put the Trace in a Data Array
     data_bar = [trace_bar];
     data_bubble = [trace_bubble];
-    range = (0, Math.max(sample_values_top10[0]))+20;
 
     //Create the Layout
     layout_bar = {
@@ -107,7 +118,7 @@ function init() {d3.json('../../samples.json').then((data)=> {
     layout_bubble = {
         title: "Sample Values vs OTU IDs",
         xaxis: {title: "OTU IDs"},
-        yaxis: {title: "Sample Values", range: range}
+        yaxis: {title: "Sample Values"}
     };
     //Show the Charts
     Plotly.newPlot('bar', data_bar, layout_bar);
@@ -117,34 +128,74 @@ function init() {d3.json('../../samples.json').then((data)=> {
 
 init();
 
-//Create the Loop to Append the Names to the Dropdown Menu
-for (i=0; i<sample_values.length; i++) {
-    d3.select("#selDataset").push(names[i]);
-}
+function optionChanged() {d3.json('../../samples.json').then((data)=> {
+    //Unpack the Values
+    var names = data.names;
+    var age = data.metadata.map(row=>row['age']);
+    var bbtype = data.metadata.map(row=>row['bbtype']);
+    var ethnicity = data.metadata.map(row=>row['ethnicity']);
+    var gender = data.metadata.map(row=>row['gender']);
+    var id = data.metadata.map(row=>row['id']);
+    var location = data.metadata.map(row=>row['location']);
+    var wfreq = data.metadata.map(row=>row['wfreq']);
+    var s_id = data.samples.map(row=>row['id']);
+    var otu_ids = data.samples.map(row=>row['otu_ids']);
+    var otu_labels = data.samples.map(row=>row['otu_labels']);
+    var sample_values = data.samples.map(row=>row['sample_values']);
 
-//Create the Loop to Go Through Each Row
-for (i=0; i<sample_values.length; i++) {
-    // Call updatePlotly() when a change takes place to the DOM
-    d3.selectAll("#selDataset").on("change", updatePlotly);
+    //Sort the Values by sample_values
+    var sorted = data.samples.sort((a, b) => b['sample_values']-a['sample_values']);
 
-    // This function is called when a dropdown menu item is selected
-    function updatePlotly() {
-    // Use D3 to select the dropdown menu
-    var dropdownMenu = d3.select("#selDataset");
-    // Assign the value of the dropdown menu option to a variable
-    var dataset = dropdownMenu.property("value");
+    //Return the Slices of Top 10 sample_values and their Corresponding otu_ids
+    var sample_values_top10 = sorted.map(row=>row['sample_values'].slice(0,10));
+    var otu_ids_top10 = sorted.map(row=>row['otu_ids'].slice(0,10).map((element)=>`OTU ${element}`));
+    var otu_ids_top10_1 = sorted.map(row=>row['otu_ids'].slice(0,10));
+    var otu_labels_top10 = sorted.map(row=>row['otu_labels'].slice(0,10));
+    console.log(sample_values_top10);
+    console.log(otu_ids_top10);
 
-    // Initialize x and y arrays
-    var x = [];
-    var y = [];
+    //Create the Loop to Append the IDs to the Dropdown Menu
+    for (i=0; i<id.length; i++) {
+        var option = d3.select("#selDataset").append('option');
+        option.attr('value', id[i]);
+        option.html(id[i]);
+    };
+    console.log(`The option is ${option}`);
+    console.log(`The option is ${option.attr('value', id[i])}`);
+    console.log(`The option is ${option.text(id[i])}`);
 
-    if (dataset === names[i]) {
-        x = sample_values[i];
-        y = otu_ids[i];
-    }
+    //Create the Loop to Go Through Each Row
+    for (i=0; i<names.length; i++) {
+        // Call updatePlotly() when a change takes place to the DOM
+        d3.selectAll("#selDataset").on("change", updatePlotly);
 
-    // Note the extra brackets around 'x' and 'y'
-    Plotly.restyle("plot", "x", [x]);
-    Plotly.restyle("plot", "y", [y]);
-    }
-};
+        // This function is called when a dropdown menu item is selected
+        function updatePlotly() {
+        // Use D3 to select the dropdown menu
+        var dropdownMenu = d3.select("#selDataset");
+        // Assign the value of the dropdown menu option to a variable
+        var dataset = dropdownMenu.property("value");
+
+        // Initialize x and y arrays
+        var x1 = [];
+        var y1 = [];
+        var x2 = [];
+        var y2 = [];
+
+        if (dataset === names[i]) {
+            x1 = sample_values_top10[i];
+            y1 = otu_ids_top10[i];
+            x2 = otu_ids[i];
+            y2 = sample_values[i];
+        };
+
+        // Note the extra brackets around 'x' and 'y'
+        Plotly.restyle("bar", "x", [x1]);
+        Plotly.restyle("bar", "y", [y1]);
+        Plotly.restyle("bubble", "x", [x2]);
+        Plotly.restyle("bubble", "y", [y2]);
+        };
+    };
+})};
+
+optionChanged();
